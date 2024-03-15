5a14a50237c95d1d20086b6447de400b4c388c7d
Kyle Keiper <kkeiper@starkstate.edu>
2024-03-15 10:13:46 -0400

    removed confusing import that didnt exist, but also didnt cause issues because vite wrote it out

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,12 +1,12 @@
 import {World} from "ape-ecs";
 
-import Mesh from "./components/Mesh.ts";
 import RenderSystem from "./systems/RenderSystem.ts";
 import UpdateSystem from "./systems/UpdateSystem.ts";
+
+import Mesh from "./components/Mesh.ts";
 import Transform from "./components/Transform.ts";
 import MetaValues from "./components/MetaValues.ts";
 import Material from "./components/Material.ts";
-import IndexedMesh from "./components/IndexedMesh.ts";
 
 
 const world = new World();
