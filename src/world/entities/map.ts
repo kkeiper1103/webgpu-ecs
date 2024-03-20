ea1ed9d74f8073ffe1d081477c1d3e35b6a9bf50
Kyle Keiper <kkeiper@starkstate.edu>
2024-03-20 08:01:44 -0400

    removed super large grass texture and used smaller sized one

--- src/world/entities/map.ts
+++ src/world/entities/map.ts
@@ -12,37 +12,36 @@ let idx = (x: number, z: number): number => z * MAP_SIZE + x;
 const BASE_ELEVATION = 2;
 
 const heights: number[] = [];
-const STEP_SIZE = .25;
-
-for(let z=0; z < MAP_SIZE; z += STEP_SIZE) {
-    for(let x = 0; x < MAP_SIZE; x += STEP_SIZE) {
+for(let z=0; z < MAP_SIZE; z += 1) {
+    for(let x = 0; x < MAP_SIZE; x += 1) {
         let idx = z * MAP_SIZE + x;
 
         let elevation = BASE_ELEVATION +
             (     1 * mapgen(x / 10, z / 10)
-                        +  .5 * mapgen(2 * x / 10, 2 * z / 10)
-                        + .25 * mapgen(4 * x / 10, 4 * z / 10) ) / (1 + .5 + .25);
+              +  .5 * mapgen(2 * x / 10, 2 * z / 10)
+              + .25 * mapgen(4 * x / 10, 4 * z / 10) ) / (1 + .5 + .25);
 
 
-        // elevation = Math.pow(elevation, .6868);
-
+        elevation = Math.pow(elevation, .87);
 
         heights[idx] = elevation - BASE_ELEVATION;
     }
 }
 
-import grass from "@assets/images/green-grass-background.jpg?url"
+import grass from "@assets/images/grass.png?url"
+import {EntityId} from "@jakeklassen/ecs";
 
-function createMapTerrain(seed: number) {
+function createMapTerrain(seed: number): EntityId {
     const e = world.createEntity();
 
     let positions: number[] = [], uvs: number[] = [], colors: number[] = [], indices: number[] = [];
 
-
     let start = 0;
-    for(let z=0; z < MAP_SIZE - 1; z++) {
-        for(let x=0; x < MAP_SIZE - 1; x++) {
+    // @todo introduce subdivision of mesh to allow for finer grain triangles.
+    for(let z=0; z < MAP_SIZE; z++) {
+        for(let x=0; x < MAP_SIZE; x++) {
 
+            // Now use finalHeight for your vertex height at position (realX, realZ)
             indices.push(
                 start, start + 1, start + 2,
                 start, start + 2, start + 3,
@@ -50,10 +49,10 @@ function createMapTerrain(seed: number) {
             start += 4;
 
             positions.push(
-                x, heights[idx(x, z)], z,
-                x + 1, heights[idx(x + 1, z)], z,
-                x + 1, heights[idx(x + 1, z + 1)], z + 1,
-                x, heights[idx(x, z + 1)], z + 1,
+                x,      heights[idx(x, z)],                z,
+                x + 1,  heights[idx(x + 1, z)],         z,
+                x + 1,  heights[idx(x + 1, z + 1)],  z + 1,
+                x,      heights[idx(x, z + 1)],         z + 1,
             );
 
             uvs.push(
@@ -109,7 +108,6 @@ function createMapTerrain(seed: number) {
 
     let image = new Image();
     image.src = grass
-    console.log(image);
     image.decode().then(() => {
         const actual = new Material({
             image,
@@ -125,8 +123,6 @@ function createMapTerrain(seed: number) {
                 addressModeU: "repeat"
             }
         });
-
-        console.log('setting new material')
 
         world.removeEntityComponents(e, Material);
         world.addEntityComponents(e, actual);
